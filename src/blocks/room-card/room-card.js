088a4408fb83b50080e02116bb80d50d0351749f
Oleg Webonlife <oleg@webonlife.ru>
2020-01-23 12:59:25 +0300

    block room-card

---
+++ src/blocks/room-card/room-card.js
@@ -0,0 +1,11 @@
+import '/var/www/html/fsd_practice/node_modules/lightslider/dist/css/lightslider.min.css';
+import '../../../node_modules/lightslider/dist/js/lightslider.min.js'
+
+$(document).ready(function() {
+  $(".light-slider").lightSlider({
+    item: 1,
+    loop: true,
+    pause: 4000,
+    controls: false,
+  });
+});
