ccba6dca2d5cf4483da48a240d47d057f275650b
Oleg Khiretdinov <oleg@oleg.win>
2020-12-01 11:31:20 +0300

    refactor: block dropdown-input

--- src/blocks/dropdown-input/dropdown-input.js
+++ src/blocks/dropdown-input/dropdown-input.js
@@ -2,4 +2,5 @@ const dropDownInputs = document.querySelectorAll('.dropdown-input');
 
 dropDownInputs.forEach((item) => {
   item.addEventListener('click', (e)=>console.log(e.target))
+  document.body.addEventListener('click', () => console.log('BODY'))
 })
